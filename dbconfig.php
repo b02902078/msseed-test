138e8d222bdae4fea7b81dc860a7c7cd817cc209
Chelsea Hu <chelsea960305@gmail.com>
2016-08-15 00:54:18 +0800

    change password

--- dbconfig.php
+++ dbconfig.php
@@ -3,8 +3,8 @@ function connect()
 {
 $host = "msseed-server.database.windows.net";
 $user = "msseed";
-$pwd = "msseed";
-$db = "12345678ms!";
+$pwd = "12345678ms!";
+$db = "msseed";
 try{
 	$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
 	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );

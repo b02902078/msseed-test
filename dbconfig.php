7c41f245068f411820fcecbbc3e97655d87fece2
Chelsea Hu <chelsea960305@gmail.com>
2016-08-15 22:58:27 +0800

    pdo

--- dbconfig.php
+++ dbconfig.php
@@ -5,7 +5,7 @@ User Name: msseed
 
 PHP Data Objects(PDO) Sample Code:*/
 
-try {
+/*try {
    $conn = new PDO ( "sqlsrv:server = tcp:msseed-server.database.windows.net,1433; Database = msseed", "msseed", "12345678ms!");
        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
        print("successfully connect")
@@ -13,5 +13,18 @@ try {
    catch ( PDOException $e ) {
       print( "Error connecting to SQL Server." );
          die(print_r($e));
-     }
+     }*/
+
+    $host = "msseed-server.database.windows.net";
+	$user = "msseed";
+	$pwd = "12345678ms!";
+	$db = "msseed";
+	try{
+		$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		print_r("successfully connect")
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
 ?>

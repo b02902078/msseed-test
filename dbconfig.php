14d0b0e32147fe25af7f6b0eeb36165567edd661
Chelsea Hu <chelsea960305@gmail.com>
2016-08-15 11:38:40 +0800

    change connect method

--- dbconfig.php
+++ dbconfig.php
@@ -6,12 +6,12 @@ $user = "msseed";
 $pwd = "12345678ms!";
 $db = "msseed";
 
-try {\r\n
-$conn = new PDO ( "sqlsrv:server = tcp:msseed-server.database.windows.net,1433; Database = msseed", "msseed", $pwd);
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-catch ( PDOException $e ) {
-  print( "Error connecting to SQL Server." );  die(print_r($e));}
-
-return $conn;
-}
+try{
+		$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
+	return $conn;
 ?>

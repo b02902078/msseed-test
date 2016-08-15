e54d5008b4038a941e63e7305ef50e41f18ac30f
Chelsea Hu <chelsea960305@gmail.com>
2016-08-15 11:50:15 +0800

    sqlsrv

--- dbconfig.php
+++ dbconfig.php
@@ -6,13 +6,19 @@ $user = "msseed";
 $pwd = "12345678ms!";
 $db = "msseed";
 
-try{
-		$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	echo "connect success";
+try  
+        {  
+            $serverName = "tcp:msseed-server.database.windows.net,1433";  
+            $connectionOptions = array("Database"=>$db,  
+                "Uid"=>$user, "PWD"=>$pwd);  
+            $conn = sqlsrv_connect($serverName, $connectionOptions);  
+            if($conn == false)  
+                die(FormatErrors(sqlsrv_errors()));  
+        }  
+        catch(Exception $e)  
+        {  
+            echo("Error!");  
+        } 
+        echo("connect success"); 
 	return $conn;
 ?>

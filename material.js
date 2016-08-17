6dcc2710cf2c308cc8026f85bb06df1c5588c14c
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-18 07:28:47 +0800

    alert result

--- material.js
+++ material.js
@@ -1,6 +1,10 @@
 var main = function(){
-    $('.ok-btn').click(function(){
-        alert("Are you sure?");
+    $('.modal .btn-danger').click(function(){
+        var data = {'component': $(this).parent().closest('.modal').attr('id')};
+        $.post('composeResource.php', data)
+        .done(function( result ) {
+    alert( "Data Loaded: " + result );
+  });
     });
 };
 

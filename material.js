2b562484c090d846fe53296bb7939d842187dd53
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-18 07:53:09 +0800

    data.component

--- material.js
+++ material.js
@@ -1,7 +1,8 @@
 var main = function(){
     $('.modal .btn-danger').click(function(){
         var data = { component: $(this).parent().closest('.modal').attr('id') };
-        alert( "Data Loaded: " + data );
+
+        alert( "Data Loaded: " + data.component );
         /*$.post("composeResource.php", data)
         .done(function( result ) {
     alert( "Data Loaded: " + result );

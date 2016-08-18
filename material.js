3d2a877923b731e1caea4b11518037b6159af992
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-18 08:03:25 +0800

    short post

--- material.js
+++ material.js
@@ -2,18 +2,11 @@ var main = function(){
     $('.modal .btn-danger').click(function(){
         var data = { component: $(this).parent().closest('.modal').attr('id') };
 
-        /*$.post("composeResource.php", data)
+        $.post("composeResource.php", data)
         .done(function( result ) {
     alert( "Data Loaded: " + result );
-  });*/
-  	$.ajax({
-    type: 'POST',
-    url: 'composeResource.php',
-    data: { component: $(this).parent().closest('.modal').attr('id') },
-    success: function(data){
-       alert(data);
-    }
   });
+  	
     });
 };
 

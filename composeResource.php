f3b22bf3768b7d8f65c57617ed8ce25ad4f2650f
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-19 00:28:36 +0800

    add $ for i

--- composeResource.php
+++ composeResource.php
@@ -19,10 +19,10 @@ try
 		$compose_function = getComposeFunction($conn, $component);
 		echo $compose_function[0][1];
 		if(!empty($compose_function)) 
-		{}
-			/*for ($i = 1; $compose_function[0][i] !== '0'; $i++) 
-			{
-				$material = $compose_function[0][i];
+		{
+			for ($i = 1; $compose_function[0][$i] !== '0'; $i++) 
+			{}}
+				/*$material = $compose_function[0][i];
 				echo $material;
 				i++;
 				$amount = $compose_function[0][i];

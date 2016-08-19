3bd600729b4805a931fed316a62a97a8b02fe227
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-19 10:57:25 +0800

    rain map

--- map.php
+++ map.php
@@ -7,7 +7,7 @@
   <!-- Bootstrap Core CSS -->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <!-- CSS Style -->
-  <link rel="stylesheet" type="text/css" href="css/map.css">
+  <!--<link rel="stylesheet" type="text/css" href="css/map.css">-->
   <link rel="stylesheet" type="text/css" href="css/rain_map.css">
 </head>
 

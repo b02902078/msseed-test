6a9162cca8aaafc8a1accfa141896f226075093b
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-21 17:19:24 +0800

    trans wrapper

--- material.php
+++ material.php
@@ -611,9 +611,9 @@ $_SESSION["s_team"] = 'B'; //testing
       <!-- Transportation Tab -->
       <div id="transport" class="tab-pane fade">
         <div class="row">
-          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper component-wrapper" data-toggle="modal" data-target="#aladdin_carpet">
+          <div class="col-md-offset-2 col-xs-offset-0 col-md-2 col-xs-3 wrapper transport-wrapper" data-toggle="modal" data-target="#aladdin_carpet">
             <img src="png/s_component/control_panel.png" class="img-responsive icon-components" alt="控制面板">
-            <p class="name component-name text-center">魔法石</p>
+            <p class="name transport-name text-center">魔法石</p>
           </div>
           <div class="col-md-2 col-xs-3 wrapper component-wrapper" data-toggle="modal" data-target="#control_panel">
             <div class="count component-count"><?php if(!empty($items)) { echo $items[0][18]; } ?></div>

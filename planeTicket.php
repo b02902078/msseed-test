6c4009b739b2ebcef9da8cddab5f161694dd8dfc
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-23 08:32:19 +0800

    adjust img size

--- planeTicket.php
+++ planeTicket.php
@@ -1,4 +1,4 @@
-<div class="container col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2">
+<div class="container col-lg-4 col-lg-offset-4 col-xs-8 col-xs-offset-2">
 	<!-- Banner -->
 	<div class="banner">
 		<img src="png/ms_airline.png" class="img-responsive"/>

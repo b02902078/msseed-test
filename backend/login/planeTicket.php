fede08ea839d9bbc9dd96899586721c1b10b48dd
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-24 00:05:28 +0800

    empty msg

--- backend/login/planeTicket.php
+++ backend/login/planeTicket.php
@@ -6,8 +6,10 @@
 
 	<!-- Get User Info -->
 	<?php
-		require_once("../taskmodel.php");
-		$user = getUserInfo($_SESSION['ID']);
+	require_once("../taskmodel.php");
+	$user = getUserInfo($_SESSION['ID']);
+	if (!empty($user)) 
+	{
 		$_SESSION['ch_name'] = $user[0][1];
 		$_SESSION['en_name'] = $user[0][2];
 		$_SESSION['country'] = $user[0][3];
@@ -20,6 +22,8 @@
 		$_SESSION['s_team'] = $user[0][1];
 
 		echo $_SESSION['en_name'] . $_SESSION['country'];
+	}
+	else { echo 'empty'; }
 	?>
 
 	<!-- Plane Ticket-->
@@ -34,13 +38,13 @@
 
 <?php
 echo "Hello ".$_SESSION['ID']."<br>";
-	echo "Your authority is  ".$_SESSION['authority']."<br>";
-	echo '  
-    <div class="container col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2">
-      <!-- Logout form-->
-      <form role="form" action="backend/login/logout.php" method="post" accept-charset="UTF-8" autocomplete="on">
-        <button type="submit" class="btn btn-default">Logout</button>
-      </form>
-    </div>';
+echo "Your authority is  ".$_SESSION['authority']."<br>";
+echo '  
+<div class="container col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2">
+	<!-- Logout form-->
+	<form role="form" action="backend/login/logout.php" method="post" accept-charset="UTF-8" autocomplete="on">
+		<button type="submit" class="btn btn-default">Logout</button>
+	</form>
+</div>';
 
 ?>

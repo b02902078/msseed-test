182d5772e416dbabf4fe0a9c32d0b59974e42978
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-24 00:09:43 +0800

    remove ticket

--- backend/login/planeTicket.php
+++ backend/login/planeTicket.php
@@ -6,7 +6,29 @@
 		<img src="png/ms_airline.png" class="img-responsive"/>
 	</div>
 
+	<!-- Get User Info -->
+	<?php
+	require_once("../taskmodel.php");
+	$user = getUserInfo($_SESSION['ID']);
+	if (!empty($user)) 
+	{
+		$_SESSION['ch_name'] = $user[0][1];
+		$_SESSION['en_name'] = $user[0][2];
+		$_SESSION['country'] = $user[0][3];
+		$_SESSION['room'] = $user[0][4];
+		$_SESSION['master'] = $user[0][5];
+		$_SESSION['mission'] = $user[0][6];
 
+		/* Decide teams for Taipei & Stronghold */
+		$_SESSION['t_team'] = $user[0][1];
+		$_SESSION['s_team'] = $user[0][1];
+
+		echo $_SESSION['en_name'] . $_SESSION['country'];
+	}
+	else { echo 'empty'; }
+	?>
+
+	
 </div>
 
 

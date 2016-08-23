e16a2ec8c9e442c9f5e02c2377e7743721bac387
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-24 01:32:17 +0800

    font

--- backend/login/planeTicket.php
+++ backend/login/planeTicket.php
@@ -8,74 +8,76 @@
 
 	<!-- Get User Info -->
 	<?php
-	require_once('/backend/taskmodel.php');
-	$user = getUserInfo($_SESSION['ID']);
-	if (!empty($user)) 
-	{
-		$_SESSION['ch_name'] = $user[0][1];
-		$_SESSION['en_name'] = $user[0][2];
-		$_SESSION['country'] = $user[0][3];
-		$_SESSION['room'] = $user[0][4];
-		$_SESSION['master'] = $user[0][5];
-		$_SESSION['mission'] = $user[0][6];
-
-		/* Decide teams for Taipei & Stronghold */
-		switch($_SESSION['country'])
+	if($_SESSION['ch_name'] == null) { // if user info is unset
+		require_once('/backend/taskmodel.php');
+		$user = getUserInfo($_SESSION['ID']);
+		if (!empty($user)) 
 		{
-			case 'America':
+			$_SESSION['ch_name'] = $user[0][1];
+			$_SESSION['en_name'] = $user[0][2];
+			$_SESSION['country'] = $user[0][3];
+			$_SESSION['room'] = $user[0][4];
+			$_SESSION['master'] = $user[0][5];
+			$_SESSION['mission'] = $user[0][6];
+
+			/* Decide teams for Taipei & Stronghold */
+			switch($_SESSION['country'])
+			{
+				case 'America':
 				$_SESSION['t_team'] = 1;
 				$_SESSION['s_team'] = 'A';
 				break;
-			case 'Canada':
+				case 'Canada':
 				$_SESSION['t_team'] = 2;
 				$_SESSION['s_team'] = 'A';
 				break;
-			case 'Brazil':
+				case 'Brazil':
 				$_SESSION['t_team'] = 3;
 				$_SESSION['s_team'] = 'A';
 				break;
-			case 'Italy':
+				case 'Italy':
 				$_SESSION['t_team'] = 4;
 				$_SESSION['s_team'] = 'B';
 				break;
-			case 'Germany':
+				case 'Germany':
 				$_SESSION['t_team'] = 5;
 				$_SESSION['s_team'] = 'B';
 				break;
-			case 'England':
+				case 'England':
 				$_SESSION['t_team'] = 6;
 				$_SESSION['s_team'] = 'B';
 				break;
-			case 'Taiwan':
+				case 'Taiwan':
 				$_SESSION['t_team'] = 7;
 				$_SESSION['s_team'] = 'C';
 				break;
-			case 'Japan':
+				case 'Japan':
 				$_SESSION['t_team'] = 8;
 				$_SESSION['s_team'] = 'C';
 				break;
-			case 'Thailand':
+				case 'Thailand':
 				$_SESSION['t_team'] = 9;
 				$_SESSION['s_team'] = 'C';
 				break;
-			case 'SouthAfrica':
+				case 'SouthAfrica':
 				$_SESSION['t_team'] = 10;
 				$_SESSION['s_team'] = 'D';
 				break;
-			case 'Madagascar':
+				case 'Madagascar':
 				$_SESSION['t_team'] = 11;
 				$_SESSION['s_team'] = 'D';
 				break;
-			case 'Egypt':
+				case 'Egypt':
 				$_SESSION['t_team'] = 12;
 				$_SESSION['s_team'] = 'D';
 				break;
-		}	
-	}
-	else
-	{
-		session_unset();
-		header('Location: /index.php');
+			}	
+		}
+		else
+		{
+			session_unset();
+			header('Location: /index.php');
+		}
 	}
 	?>
 
@@ -83,6 +85,18 @@
 	<!-- Plane Ticket-->
 	<div class="plane-ticket">
 		<?php echo '<img src="/png/plane_ticket/'.$_SESSION['country'].'.png" class="img-responsive">'; ?>
+		<!-- Larger Information -->
+		<div class="large-info" id="ID-lg"><?php echo $_SESSION['ID'] ?></div>
+		<div class="large-info" id="ch-name-lg"><?php echo $_SESSION['ch_name'] ?></div>
+		<div class="large-info" id="en-name-lg"><?php echo $_SESSION['en_name'] ?></div>
+		<div class="large-info" id="country-lg"><?php echo $_SESSION['country'] ?></div>
+		<div class="large-info" id="room-lg"><?php echo $_SESSION['room'] ?></div>
+		<!-- Smaller Information -->
+		<div class="small-info" id="ID-sm"><?php echo $_SESSION['ID'] ?></div>
+		<div class="small-info" id="ch-name-sm"><?php echo $_SESSION['ch_name'] ?></div>
+		<div class="small-info" id="en-name-sm"><?php echo $_SESSION['en_name'] ?></div>
+		<div class="small-info" id="country-sm"><?php echo $_SESSION['country'] ?></div>
+		<div class="small-info" id="room-sm"><?php echo $_SESSION['room'] ?></div>
 	</div>
 
 </div>

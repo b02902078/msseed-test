bb1cec2bf20fad93ab945733aa3b99c656ca2a72
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-26 07:59:51 +0800

    add ;

--- backend/login/changePassword.php
+++ backend/login/changePassword.php
@@ -7,7 +7,7 @@ if (!isset($_POST['pwd1']) || !isset($_POST['pwd2'])) { return "欄位空白"; }
 
 $pwd = $_POST['pwd1'];
 $pwd2 = $_POST['pwd2'];
-if ($pwd != $pwd2) { return "兩欄位不同" }
+if ($pwd != $pwd2) { return "兩欄位不同"; }
 
 $sql = "UPDATE account SET password ='".$pwd."' WHERE pid='".$_SESSION['ID']."'";
 $stmt = $conn->prepare($sql);

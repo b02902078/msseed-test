bf065bf77b3e940cc39fa13eb967a202980452d2
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-25 12:16:48 +0800

    redirect

--- backend/stronghold/updateResource.php
+++ backend/stronghold/updateResource.php
@@ -8,8 +8,7 @@ $resource = $_POST['resource'];
 $value = isset($_POST['value']) ? $_POST['value'] : 0;
 
 $sql = "UPDATE resource SET ".$resource."='".$value."' WHERE team='".$team."'";
-echo $sql;
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-
+header('Location: ../../superAdmin.php');
 ?>

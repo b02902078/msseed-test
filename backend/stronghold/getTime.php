9b45f620dba2db19f279cbc4f98e6266c42d1494
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-25 16:04:16 +0800

    echo

--- backend/stronghold/getTime.php
+++ backend/stronghold/getTime.php
@@ -5,9 +5,9 @@
     if (!empty($day)) 
     { 
     	$_SESSION["time"] = $day[0][1];
-    	if ($_SESSION["time"] <= 10) { return 'true'; }
-    	else { return 'false';  } 
+    	if ($_SESSION["time"] <= 10) { echo 'true'; }
+    	else { echo 'false';  } 
 	}
-    else { return 'false'; }
+    else { echo 'false'; }
 
 ?>

6d514ab50a2bf935def9a7bc1c340aec837c8e35
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-26 08:31:47 +0800

    syntax

--- backend/login/changePassword.php
+++ backend/login/changePassword.php
@@ -3,7 +3,7 @@ session_start();
 require_once "../taskmodel.php";
 $conn = connect();
 
-if ($_POST['pwd1'] == "" || $_POST['pwd2']) == "") { echo "欄位空白"; return; }
+if (($_POST['pwd1'] == "") || ($_POST['pwd2'] == "")) { echo "欄位空白"; return; }
 
 $pwd = $_POST['pwd1'];
 $pwd2 = $_POST['pwd2'];

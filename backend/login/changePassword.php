1af6a384d74eeaef77fbe9905a2fb71e92938a89
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-26 08:26:05 +0800

    remove function

--- backend/login/changePassword.php
+++ backend/login/changePassword.php
@@ -3,18 +3,14 @@ session_start();
 require_once "../taskmodel.php";
 $conn = connect();
 
-function changePwd() {
-	if (!isset($_POST['pwd1']) || !isset($_POST['pwd2'])) { return "欄位空白"; }
+if (!isset($_POST['pwd1']) || !isset($_POST['pwd2'])) { echo "欄位空白"; return; }
 
-	$pwd = $_POST['pwd1'];
-	$pwd2 = $_POST['pwd2'];
-	if ($pwd != $pwd2) { return "兩欄位不同"; }
+$pwd = $_POST['pwd1'];
+$pwd2 = $_POST['pwd2'];
+if ($pwd != $pwd2) { echo "兩欄位不同"; return; }
 
-	$sql = "UPDATE account SET password ='".$pwd."' WHERE pid='".$_SESSION['ID']."'";
-	$stmt = $conn->prepare($sql);
-	if ($stmt->execute() == 1) { return "SUCCESS"; }
-	else { return "FAIL"; }
-}
-
-echo changePwd();
+$sql = "UPDATE account SET password ='".$pwd."' WHERE pid='".$_SESSION['ID']."'";
+$stmt = $conn->prepare($sql);
+if ($stmt->execute() == 1) { echo "SUCCESS"; return; }
+else { echo "FAIL"; return; }
 ?>

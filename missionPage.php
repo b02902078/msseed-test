b0ae8d94d60999952b88842867b655026663bfc1
Chelsea Hu <ts2hu@uwaterloo.ca>
2016-08-28 10:06:28 +0800

    change style

--- missionPage.php
+++ missionPage.php
@@ -34,8 +34,8 @@ if($_SESSION['ID'] != null){
 
 	<div class="container col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2 text-center">
       <!-- Title -->
-    <div class="row" align="middle">
-		<div class="title">Mission</div>
+    <div class="row title">
+		Mission
 	</div>  
       <!-- Login form-->
       <form role="form" action="getMission.php" method="post" id="mission-form" autocomplete="on">
